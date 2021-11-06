daf3951f53aca6766f16329af0341fe8d34ef1f5
SUNGJUNGHOON <corqkrtk1@gmail.com>
2021-11-06 14:53:54 +0900

    백준 알고리즘 추가

--- src/main/java/baekjoon/q2920/Main.java
+++ src/main/java/baekjoon/q2920/Main.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int[] scale = new int[8];
+        String a = "";
 
         for (int i = 0; i < scale.length; i++) {
             scale[i] = scanner.nextInt();
         }
 
-        scanner.close();
+        for (int i = 0; i < scale.length - 1; i++) {
+            if (scale[i + 1] == scale[i] + 1) {
+                a = "ascending";
+            }
 
+            else if (scale[i + 1] == scale[i] - 1) {
+                a = "descending";
+            }
 
+            else {
+                a = "mixed";
+                break;
+            }
+        }
+        System.out.println(a);
     }
 }
